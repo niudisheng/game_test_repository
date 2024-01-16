e11a5a1f38bf88d33623f05c07a07feee6701574
Heimaofd <142678904+Heimaofd@users.noreply.github.com>
2024-01-16 11:44:08 +0800

    障碍物随机生成
    可以调整频率和速度

---
+++ Assets/script/player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //一些道具对应的功能是否可以使用的bool值
+   public bool unbeatable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);//设计分辨率
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ 
+}
