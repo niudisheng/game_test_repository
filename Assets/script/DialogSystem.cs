928de7d2a4c19a43f899581b6d838c198cb10328
niudisheng <106807597+niudisheng@users.noreply.github.com>
2024-01-17 14:13:14 +0800

    对话框
    split函数有问题，问题无解，没办法

--- Assets/script/DialogSystem.cs
+++ Assets/script/DialogSystem.cs
@@ -1,40 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.UIElements;
 
 public class DialogSystem : MonoBehaviour
 {
     [Header("UI组件")]
-    public Text textName;
+    public GameObject talk_ui;
     public Text textLabel;
-    public Image faceImage;
-
+    Image faceImage;
+    TMPro.TextMeshPro textMeshPro;
 
     [Header("UI组件")]
     public TextAsset textFile;
     public int index;
+    public int max_index= 0;
     List<string> text_list = new List<string>();
     void Start()
     {
-        GetText(textLabel);
+        talk_ui.SetActive(false);
+        max_index = GetText(textFile);
+        index = 0;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log(talk_ui.activeSelf);
+            if(!talk_ui.activeSelf)
+            {
+                talk_ui.SetActive(true);
+            }
+            Debug.Log($"index:{index}");
+            updateText(index);
+            index += 1;
+        }
     }
-    public void updateText(string _name,string _text)
+    public void updateText(int index)
+    {
+        textLabel.text=text_list[index];
+    }
+    public int GetText(TextAsset textFile)
     {
         text_list.Clear();
-        text_list.Add(_name);
-
-    }
-    public void GetText(Text textLabel)
-    {
-        var row=textFile.text.Split("\n");
-        Debug.Log(row[0]);
+        var rows = textFile.text.Split('\n');
+        foreach (var row in rows)
+        {
+            string[] row_list = row.Split(',');
+            var sign = row_list[0];
+            Debug.Log(row_list[0]);
+            text_list.Add(row_list[0]);
+        }
+        return text_list.Count;
     }
 }
