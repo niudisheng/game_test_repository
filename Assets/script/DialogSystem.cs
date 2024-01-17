01626666307d8022ff7df5dd30a2baf6d4dc2934
niudisheng <106807597+niudisheng@users.noreply.github.com>
2024-01-17 18:12:30 +0800

    对话框 完成

--- Assets/script/DialogSystem.cs
+++ Assets/script/DialogSystem.cs
@@ -12,18 +12,22 @@ public class DialogSystem : MonoBehaviour
     public GameObject talk_ui;
     public Text textLabel;
     Image faceImage;
-    TMPro.TextMeshPro textMeshPro;
+    //TMPro.TextMeshPro textMeshPro;
 
-    [Header("UI组件")]
-    public TextAsset textFile;
+    [Header("对话参数")]
+    public TextAsset textFile;  //对话文件
     public int index;
-    public int max_index= 0;
+    public int max_index= 0;   
+    public float textSpeed = 0.1f;
     List<string> text_list = new List<string>();
+
+    bool text_finished = true;
+    Coroutine text_display;
     void Start()
     {
         talk_ui.SetActive(false);
-        max_index = GetText(textFile);
-        index = 0;
+        max_index = GetText(textFile)-1;
+        index = -1;
     }
 
     // Update is called once per frame
@@ -31,20 +35,38 @@ public class DialogSystem : MonoBehaviour
     {   
 
         if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log(talk_ui.activeSelf);
+        {            
             if(!talk_ui.activeSelf)
             {
                 talk_ui.SetActive(true);
-            }
-            Debug.Log($"index:{index}");
-            updateText(index);
-            index += 1;
+            }            
+
+            updateText();            
         }
     }
-    public void updateText(int index)
+    public void updateText()
     {
-        textLabel.text=text_list[index];
+        Debug.Log("updateText");
+        if (index < max_index)
+        {
+            if (text_finished)
+            {
+                index++;
+                text_display=StartCoroutine(setTextUI(index)); 
+                text_finished = false;
+            }
+            else 
+            { 
+                StopCoroutine(text_display);
+                textLabel.text = text_list[index];
+                
+                text_finished = true;
+            }
+        }
+        else
+        {
+            talk_ui.SetActive(false);
+        }
     }
     public int GetText(TextAsset textFile)
     {
@@ -52,11 +74,23 @@ public class DialogSystem : MonoBehaviour
         var rows = textFile.text.Split('\n');
         foreach (var row in rows)
         {
-            string[] row_list = row.Split(',');
+            string text = row.ToString();
+            string[] row_list = text.Split(',');
             var sign = row_list[0];
-            Debug.Log(row_list[0]);
-            text_list.Add(row_list[0]);
+            var content = row_list[3];
+            Debug.Log(row_list[1]);
+            text_list.Add(content);
         }
         return text_list.Count;
+    }
+    IEnumerator setTextUI(int index)
+    {   
+        textLabel.text= string.Empty;
+        for (int i = 0; i < text_list[index].Length; i++)
+        {
+            textLabel.text += text_list[index][i];
+            yield return new WaitForSeconds(textSpeed);
+        }
+        text_finished=true;
     }
 }
