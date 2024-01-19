24fd102db3db5f144f9901cbfb62dce9d0fdf36d
niudisheng <106807597+niudisheng@users.noreply.github.com>
2024-01-19 13:41:11 +0800

    对话

--- Assets/script/DialogSystem.cs
+++ Assets/script/DialogSystem.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 //using UnityEngine.UIElements;
 
 public class DialogSystem : MonoBehaviour
@@ -11,6 +13,7 @@ public class DialogSystem : MonoBehaviour
     [Header("UI组件")]
     public GameObject talk_ui;
     public Text textLabel;
+    public Text name_text;
     Image faceImage;
     //TMPro.TextMeshPro textMeshPro;
 
@@ -19,14 +22,22 @@ public class DialogSystem : MonoBehaviour
     public int index;
     public int max_index= 0;   
     public float textSpeed = 0.1f;
+
+    [Header("图片资源")]
+    public Sprite heroine;
+    public Sprite female_2;
+    Dictionary<string,Sprite>image_dic=new Dictionary<string, Sprite>();
+
+    List<string> name_list = new List<string>();
     List<string> text_list = new List<string>();
-
     static public DialogSystem instance;
-
     bool text_finished = true;
     Coroutine text_display;
-    private void Awake()
-    {
+    private void Awake()   //单例的默认写法
+    {   
+        image_dic.Clear();
+        image_dic["女主角"]=heroine;
+        image_dic["女二"]= female_2;
         if (instance != null)
         {
             Destroy(this);
@@ -66,18 +77,21 @@ public class DialogSystem : MonoBehaviour
     static public  bool updateText()   //更新输出文字
     {          
         if (instance.index < instance.max_index)   //文字内容没有播完
-        {
+        {         
             if (instance.text_finished)
             {
                 instance.index++;
-                Debug.Log($"instance.index:{instance.index}");
-                instance.text_display = instance.StartCoroutine(instance.setTextUI(instance.index)); 
+                string content = instance.text_list[instance.index];
+                instance.name_text.text = instance.name_list[instance.index];                
+                instance.text_display = instance.StartCoroutine(instance.setTextUI(content)); 
                 instance.text_finished = false;
             }
             else //文本没结束的时候再按R
             {
+                instance.name_text.text = instance.name_list[instance.index];
                 instance.StopCoroutine(instance.text_display);
-                instance.textLabel.text = instance.text_list[instance.index];
+                string content = instance.text_list[instance.index];
+                instance.textLabel.text = content;
                 instance.text_finished = true;
             }
             return true;
@@ -96,19 +110,21 @@ public class DialogSystem : MonoBehaviour
         {
             string text = row.ToString();
             string[] row_list = text.Split(',');
-            var sign = row_list[0];
-            var content = row_list[3];
-            Debug.Log(row_list[1]);
+            //string sign = row_list[0];
+            string name = row_list[2];
+            string content = row_list[3];
+            instance.name_list.Add(name);
             instance.text_list.Add(content);
         }
         return instance.text_list.Count;
     }
-     public IEnumerator setTextUI(int index)
+     public IEnumerator setTextUI(string content)
     {   
         textLabel.text= string.Empty;
-        for (int i = 0; i < text_list[index].Length; i++)
+        //string content=text_list[index];
+        for (int i = 0; i < content.Length; i++)
         {
-            textLabel.text += text_list[index][i];
+            textLabel.text += content[i];
             yield return new WaitForSeconds(textSpeed);
         }
         text_finished=true;
