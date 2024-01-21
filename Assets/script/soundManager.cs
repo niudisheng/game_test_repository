15f5d575d8f4c8837e357f2e85b2c94a2671bc2c
niudisheng <106807597+niudisheng@users.noreply.github.com>
2024-01-21 22:36:39 +0800

    选人交互，对话框摆正

--- Assets/script/soundManager.cs
+++ Assets/script/soundManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,24 @@ public class soundManager : MonoBehaviour
     public static soundManager Instance;
     private AudioSource audioManager;
     private AudioClip jumpSound;
+
+    public Sound[] musicSound, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    public void playSound(string name)
+    {
+        Sound s = Array.Find(musicSound,x=>x.name==name);
+        if (s == null)
+        {
+            Debug.Log("Not found");
+        }
+        else
+        {
+            musicSource.clip=s.clip;
+            musicSource.Play();
+        }
+    }
+
     private void Awake()
     {
         if (Instance != null)
@@ -22,8 +41,8 @@ public class soundManager : MonoBehaviour
         jumpSound = Resources.Load<AudioClip>("sound/Saint5");
     }
     //在下面写不同音效的不同函数
-   static public void jumpingSound()
+    static public void jumpingSound()
     {
-       //Instance.audioManager.PlayOneShot(Instance.jumpSound);
+        Instance.audioManager.PlayOneShot(Instance.jumpSound);
     }
 }
