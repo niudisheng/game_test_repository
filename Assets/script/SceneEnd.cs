6b7fcd4d6f7c5b68ab9da748bc000276d2404044
Yoohuan <wen666457@gmail.com>
2024-01-24 22:36:30 +0800

    zhuanchang

---
+++ Assets/script/SceneEnd.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneEnd : MonoBehaviour
+{
+    public float endTime;
+    public int sceneNum;
+    public Image endPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endPanel.fillAmount += 1f / endTime * Time.deltaTime;
+        if (endPanel.fillAmount >= 1 )
+        {
+            sceneManager.Instance.changeScene(sceneNum);
+        }
+        
+    }
+}
