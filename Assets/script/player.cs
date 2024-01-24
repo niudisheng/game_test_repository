58464cd2d1070d9471f8634ad1ce0f773f9075e3
Yoohuan <wen666457@gmail.com>
2024-01-24 20:30:50 +0800

    Update player.cs

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -7,7 +7,11 @@ public class player : MonoBehaviour
     Animator animator;
     Rigidbody2D rb;
     public float move_speed;
+    public GameObject sign;
     private Vector2 move;
+    private Vector3 scale1;
+    private Vector3 scale2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,9 @@ public class player : MonoBehaviour
         Time.timeScale = 1;
         if (move_speed == 0)
         { move_speed = 100; }
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+        scale1 = transform.localScale;
+        scale2 = sign.transform.localScale;
     }
 
     // Update is called once per frame
@@ -37,6 +43,25 @@ public class player : MonoBehaviour
         move = new(horizontal,vertical);
         
         move=move*move_speed;
-        rb.velocity = move;       
+        rb.velocity = move;
+
+
+        bool playerHasXAxisSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasXAxisSpeed)
+        {
+            if (rb.velocity.x > 0.1f)
+            {
+                transform.localScale = scale1;
+                sign.transform.localScale = scale2;
+
+            }
+
+            if (rb.velocity.x < -0.1f)
+            {
+                transform.localScale = new Vector3(-scale1.x,scale1.y,scale1.z);
+                sign.transform.localScale = new Vector3(-scale2.x,scale2.y,scale2.z);
+
+            }
+        }
     }
 }
