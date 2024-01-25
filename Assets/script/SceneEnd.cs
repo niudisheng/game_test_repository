50542278c640f5ab72ed0785fb59b27fe275404b
Yoohuan <wen666457@gmail.com>
2024-01-25 11:34:31 +0800

    new

--- Assets/script/SceneEnd.cs
+++ Assets/script/SceneEnd.cs
@@ -11,20 +11,15 @@ public class SceneEnd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        endPanel.fillAmount = 1;
-=======
+
         endPanel.fillAmount += 1f / endTime * Time.deltaTime;
-<<<<<<< HEAD
->>>>>>> parent of ed29f3fb (1)
-=======
->>>>>>> parent of ed29f3fb (1)
+
         if (endPanel.fillAmount >= 1 )
         {
             sceneManager.Instance.changeScene(sceneNum);
